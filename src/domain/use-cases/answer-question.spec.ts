8ec563882e6fd642bec66de7a89ef65ff9dbf8d3
kielkow <matheuskiel@fiorifer.com.br>
2023-10-12 17:24:38 -0300

    wip: answers repository

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,8 +1,14 @@
 import { expect, test } from 'vitest'
+
+import { Answer } from '../entities/answer'
 import { AnswerQuestionUseCase } from './answer-question'
 
+const fakeAnswersRepository = {
+    createAnswer: async (answer: Answer) => answer
+}
+
 test('create an answer', async () => {
-    const answerQuestion = new AnswerQuestionUseCase()
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
 
     const answer = await answerQuestion.execute({
         authorId: '1',
--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,19 +1,27 @@
 import { Answer } from "../entities/answer";
 
+import { AnswersRepository } from "../repositories/answers-repository";
+
 interface AnswerQuestionUseCaseRequest {
     authorId: string;
     questionId: string;
     content: string;
 }
 
-export class AnswerQuestionUseCase {    
+export class AnswerQuestionUseCase {
+    constructor(
+        private readonly answersRepository: AnswersRepository
+    ) {}
+
     async execute(
         { authorId, questionId, content }: AnswerQuestionUseCaseRequest
     ): Promise<Answer> {
-        return new Answer({ 
+        const answer = new Answer({ 
             content, 
             questionId, 
             authorId 
         });
+
+        return await this.answersRepository.createAnswer(answer);
     }
 }
