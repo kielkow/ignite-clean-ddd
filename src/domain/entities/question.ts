7de395d4ae48a5412fa13160558a8ce6b85f9774
kielkow <matheuskiel@fiorifer.com.br>
2023-10-16 11:23:15 -0300

    feat: type Optional

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -2,13 +2,30 @@ import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 import { Slug } from './value-objects/slug'
+import { Optional } from '@/core/types/optional'
 
 interface QuestionProps {
 	title: string
 	slug: Slug
 	content: string
+	difficulty: 'easy' | 'medium' | 'hard'
 	authorId: UniqueEntityID
 	bestAnswerId?: UniqueEntityID
 }
 
-export class Question extends Entity<QuestionProps> {}
+export class Question extends Entity<QuestionProps> {
+	static create(
+		props: Optional<QuestionProps, 'difficulty'>,
+		id?: UniqueEntityID,
+	) {
+		const question = new Question(
+			{
+				...props,
+				difficulty: props.difficulty ?? 'medium',
+			},
+			id,
+		)
+
+		return question
+	}
+}
