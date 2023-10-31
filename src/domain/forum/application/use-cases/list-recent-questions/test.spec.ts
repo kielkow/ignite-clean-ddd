a9c89d8a5da616e8ab0fbe94604dee6d1961337b
kielkow <matheuskiel@fiorifer.com.br>
2023-10-31 11:21:36 -0300

    test: should be able to list recent questions by pagination

--- src/domain/forum/application/use-cases/list-recent-questions/test.spec.ts
+++ src/domain/forum/application/use-cases/list-recent-questions/test.spec.ts
@@ -30,4 +30,30 @@ describe('ListRecentQuestionsUseCase', () => {
 		expect(questions[1].createdAt).toEqual(new Date('2021-01-02'))
 		expect(questions[2].createdAt).toEqual(new Date('2021-01-01'))
 	})
+
+	it('should be able to list recent questions by pagination', async () => {
+		await inMemoryQuestionsRepository.createQuestion(
+			makeQuestion({}, undefined, new Date('2021-01-01')),
+		)
+		await inMemoryQuestionsRepository.createQuestion(
+			makeQuestion({}, undefined, new Date('2021-01-02')),
+		)
+		await inMemoryQuestionsRepository.createQuestion(
+			makeQuestion({}, undefined, new Date('2021-01-03')),
+		)
+		await inMemoryQuestionsRepository.createQuestion(
+			makeQuestion({}, undefined, new Date('2021-01-04')),
+		)
+
+		const questionsFirstPage = await sut.execute({ page: 1, perPage: 2 })
+		const questionsSecondPage = await sut.execute({ page: 2, perPage: 2 })
+
+		expect(questionsFirstPage).toHaveLength(2)
+		expect(questionsFirstPage[0].createdAt).toEqual(new Date('2021-01-04'))
+		expect(questionsFirstPage[1].createdAt).toEqual(new Date('2021-01-03'))
+
+		expect(questionsSecondPage).toHaveLength(2)
+		expect(questionsSecondPage[0].createdAt).toEqual(new Date('2021-01-02'))
+		expect(questionsSecondPage[1].createdAt).toEqual(new Date('2021-01-01'))
+	})
 })
