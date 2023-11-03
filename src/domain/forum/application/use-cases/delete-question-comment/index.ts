5e95e3bc698918f719d9576d18107fc761a8ebe1
kielkow <matheuskiel@fiorifer.com.br>
2023-11-03 18:33:46 -0300

    feat: add validations before delete question comment

--- src/domain/forum/application/use-cases/delete-question-comment/index.ts
+++ src/domain/forum/application/use-cases/delete-question-comment/index.ts
@@ -1,11 +1,26 @@
 import { QuestionsCommentsRepository } from '../../repositories/questions-comments-repository'
 
+interface Input {
+	id: string
+	authorId: string
+}
+
 export class DeleteQuestionCommentUseCase {
 	constructor(
 		private readonly questionCommentRepository: QuestionsCommentsRepository,
 	) {}
 
-	async execute(id: string): Promise<void> {
+	async execute({ id, authorId }: Input): Promise<void> {
+		const questionComment = await this.questionCommentRepository.findById(id)
+
+		if (!questionComment) {
+			throw new Error('Question comment not found')
+		}
+
+		if (questionComment.authorId.id !== authorId) {
+			throw new Error('You are not allowed to delete this question comment')
+		}
+
 		await this.questionCommentRepository.delete(id)
 	}
 }
--- src/domain/forum/application/use-cases/delete-question-comment/test.spec.ts
+++ src/domain/forum/application/use-cases/delete-question-comment/test.spec.ts
@@ -17,12 +17,37 @@ describe('DeleteQuestionCommentByIdUseCase', () => {
 
 		await inMemoryQuestionsRepository.create(comment)
 
-		await sut.execute(comment.id)
+		await sut.execute({
+			id: comment.id,
+			authorId: comment.authorId.id,
+		})
 
 		const questionComment = await inMemoryQuestionsRepository.findById(
 			comment.id,
 		)
 
 		expect(questionComment).toBeUndefined()
+	})
+
+	it('should not be able to delete question comment by ID if it does not exist', async () => {
+		await expect(
+			sut.execute({
+				id: 'invalid_id',
+				authorId: 'any_author_id',
+			}),
+		).rejects.toThrow('Question comment not found')
+	})
+
+	it('should not be able to delete question comment by ID if user is not the author', async () => {
+		const comment = makeQuestionComment()
+
+		await inMemoryQuestionsRepository.create(comment)
+
+		await expect(
+			sut.execute({
+				id: comment.id,
+				authorId: 'invalid_author_id',
+			}),
+		).rejects.toThrow('You are not allowed to delete this question comment')
 	})
 })
