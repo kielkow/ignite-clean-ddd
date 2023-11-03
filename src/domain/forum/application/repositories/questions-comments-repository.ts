1be3ab63de756006e2f6cab5f3fff50f0d919fad
kielkow <matheuskiel@fiorifer.com.br>
2023-11-03 18:16:00 -0300

    feat: add news methods to question comment repository

--- src/domain/forum/application/repositories/questions-comments-repository.ts
+++ src/domain/forum/application/repositories/questions-comments-repository.ts
@@ -2,4 +2,10 @@ import { QuestionComment } from '../../enterprise/entities/question-comment'
 
 export interface QuestionsCommentsRepository {
 	create(questionComment: QuestionComment): Promise<QuestionComment>
+
+	findById(id: string): Promise<QuestionComment | undefined>
+
+	findAll(questionId: string): Promise<QuestionComment[]>
+
+	delete(id: string): Promise<void>
 }
--- src/test/repositories/in-memory-questions-comments-repository.ts
+++ src/test/repositories/in-memory-questions-comments-repository.ts
@@ -10,4 +10,22 @@ export class InMemoryQuestionsCommentsRepository
 		this.questionsComments.push(questionComment)
 		return questionComment
 	}
+
+	async findById(id: string): Promise<QuestionComment | undefined> {
+		return this.questionsComments.find(
+			(questionComment) => questionComment.id === id,
+		)
+	}
+
+	async findAll(questionId: string): Promise<QuestionComment[]> {
+		return this.questionsComments.filter(
+			(questionComment) => questionComment.questionId.id === questionId,
+		)
+	}
+
+	async delete(id: string): Promise<void> {
+		this.questionsComments = this.questionsComments.filter(
+			(questionComment) => questionComment.id !== id,
+		)
+	}
 }
