c50e136d6228a92008bf21ab7b36cbc752387f4d
kielkow <matheuskiel@fiorifer.com.br>
2023-11-15 11:28:05 -0300

    refact: refact usecases to use response handling module

--- src/domain/forum/application/use-cases/list-question-answers/test.spec.ts
+++ src/domain/forum/application/use-cases/list-question-answers/test.spec.ts
@@ -1,8 +1,11 @@
+import { Success } from '@/core/response-handling'
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
 import { makeAnswer } from '@/test/factories/make-answer'
 import { InMemoryAnswersRepository } from '@/test/repositories/in-memory-answers-repository'
 
 import { ListQuestionAnswersUseCase } from '.'
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 describe('ListQuestionAnswersUseCase', () => {
 	let inMemoryAnswersRepository: InMemoryAnswersRepository
@@ -33,7 +36,17 @@ describe('ListQuestionAnswersUseCase', () => {
 		)
 		await inMemoryAnswersRepository.createAnswer(makeAnswer())
 
-		const answers = await sut.execute(questionId.id, { page: 1, perPage: 10 })
+		const result = await sut.execute({
+			questionId: questionId.id,
+			paginationParams: {
+				page: 1,
+				perPage: 10,
+			},
+		})
+		const answers = result.getValue()
+
+		expect(Success.is(result)).toBe(true)
+		expect(result).toBeInstanceOf(Success)
 
 		expect(answers).toHaveLength(3)
 	})
@@ -62,16 +75,46 @@ describe('ListQuestionAnswersUseCase', () => {
 			}),
 		)
 
-		const answersFirstPage = await sut.execute(questionId.id, {
-			page: 1,
-			perPage: 2,
+		const firstResult = await sut.execute({
+			questionId: questionId.id,
+			paginationParams: {
+				page: 1,
+				perPage: 2,
+			},
 		})
-		const answersSecondPage = await sut.execute(questionId.id, {
-			page: 2,
-			perPage: 2,
+		const secondResult = await sut.execute({
+			questionId: questionId.id,
+			paginationParams: {
+				page: 2,
+				perPage: 2,
+			},
 		})
+
+		const answersFirstPage = firstResult.getValue()
+		const answersSecondPage = secondResult.getValue()
+
+		expect(Success.is(firstResult)).toBe(true)
+		expect(firstResult).toBeInstanceOf(Success)
+		expect(Success.is(secondResult)).toBe(true)
+		expect(secondResult).toBeInstanceOf(Success)
 
 		expect(answersFirstPage).toHaveLength(2)
 		expect(answersSecondPage).toHaveLength(2)
+	})
+
+	it('should not be able to list question answers if question does not exists', async () => {
+		const result = await sut.execute({
+			questionId: 'non-existing-question-id',
+			paginationParams: {
+				page: 1,
+				perPage: 10,
+			},
+		})
+
+		const answers = result.getValue()
+
+		expect(Success.is(result)).toBe(true)
+		expect(result).toBeInstanceOf(Success)
+		expect(answers).toHaveLength(0)
 	})
 })
