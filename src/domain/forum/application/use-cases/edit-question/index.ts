9ea4595665dfdded32336160b700c13ba5b11986
kielkow <matheuskiel@fiorifer.com.br>
2023-11-21 11:04:10 -0300

    feat: update question with attachments

--- src/domain/forum/application/use-cases/edit-question/index.ts
+++ src/domain/forum/application/use-cases/edit-question/index.ts
@@ -1,22 +1,37 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { ResponseHandling, success, fail } from '@/core/response-handling'
 
-import { ResourceNotFoundError, NotAllowedError } from '../../errors'
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
+import { QuestionAttachmentList } from '@/domain/forum/enterprise/entities/question-attachment-list'
 
 import { QuestionsRepository } from '../../repositories/questions-repository'
+import { QuestionAttachmentsRepository } from '../../repositories/question-attachments-repository'
+
+import { ResourceNotFoundError, NotAllowedError } from '../../errors'
 
 interface Input {
 	id: string
 	authorId: string
 	title: string
 	content: string
+	attachmentsIds?: string[]
 }
 
 type Output = ResponseHandling<ResourceNotFoundError | NotAllowedError, void>
 
 export class EditQuestionUseCase {
-	constructor(private readonly questionsRepository: QuestionsRepository) {}
+	constructor(
+		private readonly questionsRepository: QuestionsRepository,
+		private questionAttachmentsRepository: QuestionAttachmentsRepository,
+	) {}
 
-	async execute({ id, authorId, title, content }: Input): Promise<Output> {
+	async execute({
+		id,
+		authorId,
+		title,
+		content,
+		attachmentsIds,
+	}: Input): Promise<Output> {
 		const question = await this.questionsRepository.findById(id)
 
 		if (!question) return fail(new ResourceNotFoundError())
@@ -27,6 +42,25 @@ export class EditQuestionUseCase {
 
 		question.title = title
 		question.content = content
+
+		if (attachmentsIds) {
+			const currentAttachments =
+				await this.questionAttachmentsRepository.findByQuestionId(id)
+
+			const questionAttachmentList =
+				QuestionAttachmentList.create(currentAttachments)
+
+			const questionAttachments = attachmentsIds.map((attachmentId) => {
+				return QuestionAttachment.create({
+					attachmentId: new UniqueEntityID(attachmentId),
+					questionId: new UniqueEntityID(id),
+				})
+			})
+
+			questionAttachmentList.update(questionAttachments)
+
+			question.attachments = questionAttachmentList
+		}
 
 		await this.questionsRepository.editQuestion(question)
 
--- src/domain/forum/application/use-cases/edit-question/test.spec.ts
+++ src/domain/forum/application/use-cases/edit-question/test.spec.ts
@@ -1,18 +1,31 @@
 import { Success, Fail } from '@/core/response-handling'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
 import { makeQuestion } from '@/test/factories/make-question'
 import { InMemoryQuestionsRepository } from '@/test/repositories/in-memory-questions-repository'
+import { InMemoryQuestionAttachmentsRepository } from '@/test/repositories/in-memory-question-attachments-repository'
+
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
 
 import { NotAllowedError, ResourceNotFoundError } from '../../errors'
 
 import { EditQuestionUseCase } from '.'
 
 describe('EditQuestionUseCase', () => {
-	let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+	let inMemoryQuestionsRepository: InMemoryQuestionsRepository,
+		inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
+
 	let sut: EditQuestionUseCase
 
 	beforeEach(() => {
 		inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
-		sut = new EditQuestionUseCase(inMemoryQuestionsRepository)
+		inMemoryQuestionAttachmentsRepository =
+			new InMemoryQuestionAttachmentsRepository()
+
+		sut = new EditQuestionUseCase(
+			inMemoryQuestionsRepository,
+			inMemoryQuestionAttachmentsRepository,
+		)
 	})
 
 	it('should be able to edit an question', async () => {
@@ -67,5 +80,40 @@ describe('EditQuestionUseCase', () => {
 		expect(Fail.is(result)).toBe(true)
 		expect(result).toBeInstanceOf(Fail)
 		expect(result).toEqual({ error: expect.any(ResourceNotFoundError) })
+	})
+
+	it('should be able to edit an question attachments', async () => {
+		const question = await inMemoryQuestionsRepository.createQuestion(
+			makeQuestion(),
+		)
+
+		const payloadAttachment = QuestionAttachment.create({
+			questionId: new UniqueEntityID(question.id),
+			attachmentId: new UniqueEntityID(),
+		})
+
+		const attachment = await inMemoryQuestionAttachmentsRepository.create(
+			payloadAttachment,
+		)
+
+		const result = await sut.execute({
+			id: question.id,
+			authorId: question.authorId.id,
+			title: 'Edit Title',
+			content: 'Edit Content',
+			attachmentsIds: [attachment.id],
+		})
+
+		const questionEdited = await inMemoryQuestionsRepository.findById(
+			question.id,
+		)
+
+		expect(questionEdited).toBeTruthy()
+		expect(questionEdited?.title).toBe('Edit Title')
+		expect(questionEdited?.content).toBe('Edit Content')
+		expect(questionEdited?.attachments.getItems()).toHaveLength(1)
+
+		expect(Success.is(result)).toBe(true)
+		expect(result).toBeInstanceOf(Success)
 	})
 })
