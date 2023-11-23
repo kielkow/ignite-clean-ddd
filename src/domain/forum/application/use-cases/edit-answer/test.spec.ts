a2a98cd39553433d61b160cd8c5095ea6aa89023
kielkow <matheuskiel@fiorifer.com.br>
2023-11-22 22:46:54 -0300

    feat: edit question with attachments

--- src/domain/forum/application/use-cases/edit-answer/test.spec.ts
+++ src/domain/forum/application/use-cases/edit-answer/test.spec.ts
@@ -1,19 +1,31 @@
 import { Success, Fail } from '@/core/response-handling'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
 
 import { makeAnswer } from '@/test/factories/make-answer'
 import { InMemoryAnswersRepository } from '@/test/repositories/in-memory-answers-repository'
+import { InMemoryAnswerAttachmentsRepository } from '@/test/repositories/in-memory-answer-attachments-repository'
 
 import { NotAllowedError, ResourceNotFoundError } from '../../errors'
 
 import { EditAnswerUseCase } from '.'
 
 describe('EditAnswerUseCase', () => {
-	let inMemoryAnswersRepository: InMemoryAnswersRepository
+	let inMemoryAnswersRepository: InMemoryAnswersRepository,
+		inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
+
 	let sut: EditAnswerUseCase
 
 	beforeEach(() => {
 		inMemoryAnswersRepository = new InMemoryAnswersRepository()
-		sut = new EditAnswerUseCase(inMemoryAnswersRepository)
+		inMemoryAnswerAttachmentsRepository =
+			new InMemoryAnswerAttachmentsRepository()
+
+		sut = new EditAnswerUseCase(
+			inMemoryAnswersRepository,
+			inMemoryAnswerAttachmentsRepository,
+		)
 	})
 
 	it('should be able to edit an answer', async () => {
@@ -58,5 +70,34 @@ describe('EditAnswerUseCase', () => {
 		expect(Fail.is(result)).toBe(true)
 		expect(result).toBeInstanceOf(Fail)
 		expect(result).toEqual({ error: expect.any(ResourceNotFoundError) })
+	})
+
+	it('should be able to edit an answer attachments', async () => {
+		const answer = await inMemoryAnswersRepository.createAnswer(makeAnswer())
+
+		const payloadAttachment = AnswerAttachment.create({
+			answerId: new UniqueEntityID(answer.id),
+			attachmentId: new UniqueEntityID(),
+		})
+
+		const attachment = await inMemoryAnswerAttachmentsRepository.create(
+			payloadAttachment,
+		)
+
+		const result = await sut.execute({
+			id: answer.id,
+			authorId: answer.authorId.id,
+			content: 'Edit Content',
+			attachmentsIds: [attachment.id],
+		})
+
+		const answerEdited = await inMemoryAnswersRepository.findById(answer.id)
+
+		expect(answerEdited).toBeTruthy()
+		expect(answerEdited?.content).toBe('Edit Content')
+		expect(answerEdited?.attachments.getItems()).toHaveLength(1)
+
+		expect(Success.is(result)).toBe(true)
+		expect(result).toBeInstanceOf(Success)
 	})
 })
