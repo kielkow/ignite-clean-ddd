84e0a6405d37a1ba50194e1f9efc9929a87a422f
kielkow <matheuskiel@fiorifer.com.br>
2023-11-30 19:42:17 -0300

    feat: send notification to question author when an answer was created

--- src/domain/notification/application/subscribers/on-answer-created/index.ts
+++ src/domain/notification/application/subscribers/on-answer-created/index.ts
@@ -2,9 +2,15 @@ import { DomainEvents } from '@/core/events/domain-events'
 import { EventHandler } from '@/core/events/event-handler'
 
 import { AnswerCreatedEvent } from '@/domain/forum/enterprise/events/answer-created-event'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+
+import { SendNotificationUseCase } from '../../use-cases/send-notification'
 
 export class OnAnswerCreated implements EventHandler {
-	constructor() {
+	constructor(
+		private questionsRepository: QuestionsRepository,
+		private sendNotificationUseCase: SendNotificationUseCase,
+	) {
 		this.setupSubscriptions()
 	}
 
@@ -19,6 +25,17 @@ export class OnAnswerCreated implements EventHandler {
 		event: AnswerCreatedEvent,
 	): Promise<void> {
 		const { answer } = event
-		console.log('Answer Created Notification:', answer)
+
+		const question = await this.questionsRepository.findById(
+			answer.questionId.id,
+		)
+
+		if (question) {
+			await this.sendNotificationUseCase.execute({
+				recipientId: question.authorId,
+				title: 'New Answer',
+				content: `Your question "${question.title}" has a new answer.`,
+			})
+		}
 	}
 }
--- src/domain/notification/application/subscribers/on-answer-created/test.spec.ts
+++ src/domain/notification/application/subscribers/on-answer-created/test.spec.ts
@@ -1,21 +1,66 @@
+import { SpyInstance } from 'vitest'
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+import { waitFor } from '@/test/utils/wait-for'
+
 import { makeAnswer } from '@/test/factories/make-answer'
+import { makeQuestion } from '@/test/factories/make-question'
+
 import { InMemoryAnswersRepository } from '@/test/repositories/in-memory-answers-repository'
+import { InMemoryQuestionsRepository } from '@/test/repositories/in-memory-questions-repository'
+import { InMemoryNotificationsRepository } from '@/test/repositories/in-memory-notifications-repository'
+
+import {
+	Input as SendNotificationRequest,
+	Output as SendNotificationResponse,
+	SendNotificationUseCase,
+} from '../../use-cases/send-notification'
 
 import { OnAnswerCreated } from '.'
 
 describe('OnAnswerCreated', () => {
-	let inMemoryAnswersRepository: InMemoryAnswersRepository
+	let inMemoryAnswersRepository: InMemoryAnswersRepository,
+		inMemoryQuestionsRepository: InMemoryQuestionsRepository,
+		inMemoryNotificationsRepository: InMemoryNotificationsRepository
+
+	let sendNotificationUseCase: SendNotificationUseCase
+
 	let sut: OnAnswerCreated
+
+	let spyNewAnswerNotification: SpyInstance<
+		[SendNotificationRequest],
+		Promise<SendNotificationResponse>
+	>
 
 	beforeEach(() => {
 		inMemoryAnswersRepository = new InMemoryAnswersRepository()
-		sut = new OnAnswerCreated()
+		inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+		inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+
+		sendNotificationUseCase = new SendNotificationUseCase(
+			inMemoryNotificationsRepository,
+		)
+
+		spyNewAnswerNotification = vi.spyOn(sendNotificationUseCase, 'execute')
+
+		sut = new OnAnswerCreated(
+			inMemoryQuestionsRepository,
+			sendNotificationUseCase,
+		)
+
 		console.info('Subscriber Created:', sut)
 	})
 
 	it('should be able send notification when answer is created', async () => {
-		const answer = makeAnswer()
+		const question = makeQuestion()
+		const answer = makeAnswer({ questionId: new UniqueEntityID(question.id) })
 
+		await inMemoryQuestionsRepository.createQuestion(question)
 		await inMemoryAnswersRepository.createAnswer(answer)
+
+		await waitFor(() => {
+			expect(spyNewAnswerNotification).toHaveBeenCalled()
+		}, 5000)
 	})
 })
